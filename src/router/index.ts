cf4f421727eefdb1e8375c235026f296ed104a93
YANgANG <neil_young.7up@outlook.com>
2023-02-11 14:10:27 +1030

    feat: use vue-router for new added routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,46 @@
-import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "../views/HomeView.vue"
+import { createRouter, createWebHistory } from 'vue-router'
+import TabsView from '../views/tabs/TabsView.vue'
+import HomeView from '../views/tabs/home/HomeView.vue'
+import OrdersView from '../views/tabs/orders/OrdersView.vue'
+import SettingsView from '../views/tabs/settings/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
+      path: '/',
+      redirect: 'home',
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: '/tabs',
+      name: 'tabs',
+      component: TabsView,
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          meta: {
+            title: 'Home',
+          },
+          component: HomeView,
+        },
+        {
+          path: '/orders',
+          name: 'orders',
+          meta: {
+            title: 'Orders',
+          },
+          component: OrdersView,
+        },
+        {
+          path: '/settings',
+          name: 'settings',
+          meta: {
+            title: 'Settings',
+          },
+          component: SettingsView,
+        },
+      ],
     },
   ],
 })
